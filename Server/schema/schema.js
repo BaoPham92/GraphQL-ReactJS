acdf2c6be229ee0693d1c7a9d59e898ee27aca1b
Cuza <baopham92@yahoo.com>
2019-02-12 17:57:15 -0500

    lodash, test data & base queries enabled for graphiql

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql');
+const _= require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
@@ -19,10 +20,19 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
             resolve(parent, args) {
                 // Retrieve data from db
+                return _.find(companies, {id: args.id})
             }
         }
     }
 })
+
+// Development Data
+var companies = [
+    { id: "1", name: "Name Number 1", fieldOfBusiness: "Business: Clothing" },
+    { id: "2", name: "Name Number 2", fieldOfBusiness: "Business: Sales" },
+    { id: "3", name: "Name Number 3", fieldOfBusiness: "Business: Defense" },
+    { id: "4", name: "Name Number 4", fieldOfBusiness: "Business: Media" }
+]
 
 module.exports = new GraphQLSchema({
     query: RootQuery
